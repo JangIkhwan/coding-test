9733ea66c9719063c7eb1c937bf519abb0a3dee2
Ikhwan Jang <87964632+JangIkhwan@users.noreply.github.com>
2026-05-13 16:15:36 +0900

    [level 2] Title: 튜플, Time: 26.68 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    /*
+    26/5/13 15:25 ~ 
+    
+    집합이 나타내는 튜플을 구하자
+    
+    집합의 원소의 순서는 바뀔 수 있다
+    
+    
+    */
+    public int[] solution(String s) {
+        // 문자열을 sets 리스트에 저장
+                
+        List<List<Integer>> sets = new ArrayList<>();
+        
+        s = s.substring(2, s.length() - 2);
+        int prevIndex = 0;
+        while(true){
+            int index = s.indexOf("},{", prevIndex);
+            if(index == -1){
+                break;
+            }
+            String setString = s.substring(prevIndex, index);
+            String[] numStrings = setString.split(",");
+           
+            List<Integer> set = new ArrayList<>();
+            for(String n : numStrings){
+                set.add(Integer.parseInt(n));
+            }
+            sets.add(set);
+            prevIndex = index + 3;
+        }
+        String setString = s.substring(prevIndex, s.length());
+        String[] numStrings = setString.split(",");
+        List<Integer> temp = new ArrayList<>();
+        for(String n : numStrings){
+            temp.add(Integer.parseInt(n));
+        }
+        sets.add(temp);
+        
+        
+        // System.out.println(sets);
+        
+        // sets를 길이에 대해 오름차순으로 정렬
+        sets.sort((a, b) -> a.size() - b.size());
+        
+        // sets의 각 원소에 대해서 
+            // 포함되지 않은 원소를 answer 리스트에 추가
+        List<Integer> answer = new ArrayList<>();
+        Set<Integer> seens = new HashSet<>();
+        for(List<Integer> set : sets){
+            for(int e : set){
+                if(!seens.contains(e)){
+                    answer.add(e);
+                    seens.add(e);
+                    break;
+                }
+            }
+        }
+        
+        return answer.stream().mapToInt(a -> a).toArray();
+    }
+}
